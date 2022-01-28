de5cf74418f693f9fef6fb7bc3d503c4c4e46e82
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2022-01-28 11:45:37 +0100

    bokio backup muied

--- src/pages/bokio-backup-page.tsx
+++ src/pages/bokio-backup-page.tsx
@@ -1,18 +1,18 @@
-import styled from 'styled-components/macro';
-
-const PageWrapper = styled.div``;
+import { Box, Link, Typography } from '@mui/material';
 
 const BokioBackupPage = () => {
   return (
-    <PageWrapper>
-      <h2>Bokio backup</h2>
-      <p>
+    <Box>
+      <Typography variant="h2">Bokio backup</Typography>
+      <Typography variant="body1" mt={2}>
         Om du gör din bokföring med <a href="https://www.bokio.se/">Bokio</a>.
         Kan du köra{' '}
-        <a href="https://github.com/Gyran/bokio-backup">detta lilla script</a>{' '}
+        <Link href="https://github.com/Gyran/bokio-backup">
+          detta lilla script
+        </Link>{' '}
         för att exportera all data från Bokio och ha som backup.
-      </p>
-    </PageWrapper>
+      </Typography>
+    </Box>
   );
 };
 
