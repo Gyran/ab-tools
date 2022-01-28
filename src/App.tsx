6e92f23f82258d77e96d1f481214d181f13fb916
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2022-01-28 11:40:46 +0100

    nice theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React, { PropsWithChildren, Suspense } from 'react';
 import {
   AppBar,
   CssBaseline,
@@ -9,8 +10,9 @@ import {
   ListItemText,
   Toolbar,
   Typography,
+  ThemeProvider,
 } from '@mui/material';
-import React, { PropsWithChildren, Suspense } from 'react';
+import BusinessIcon from '@mui/icons-material/Business';
 import {
   Route,
   BrowserRouter as Router,
@@ -20,6 +22,7 @@ import {
   useMatch,
 } from 'react-router-dom';
 import './App.css';
+import theme from './styling/theme';
 
 const DrawerWidth = 240;
 
@@ -56,64 +59,67 @@ const MyNavLink = (props: MyNavLinkProps) => {
 
 function App() {
   return (
-    <Router>
-      <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
-        <AppBar
-          sx={{
-            zIndex: (theme) => theme.zIndex.drawer + 1,
-          }}
-        >
-          <Toolbar>
-            <Typography variant="h6" noWrap component="div">
-              AB Tools
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          variant="permanent"
-          anchor="left"
-          sx={{
-            width: DrawerWidth,
-            flexShrink: 0,
-            '& .MuiDrawer-paper': {
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Box sx={{ display: 'flex' }}>
+          <CssBaseline />
+          <AppBar
+            sx={{
+              zIndex: (theme) => theme.zIndex.drawer + 1,
+            }}
+          >
+            <Toolbar>
+              <BusinessIcon sx={{ mr: 2 }} />
+              <Typography variant="h6" noWrap component="div">
+                AB Tools
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            variant="permanent"
+            anchor="left"
+            sx={{
               width: DrawerWidth,
-              boxSizing: 'border-box',
-            },
-          }}
-        >
-          <Toolbar />
-          <Divider />
-          <List>
-            <MyNavLink to="/">Hem</MyNavLink>
-            {/* <MyNavLink to="/invoice-qr">Invoice QR</MyNavLink> */}
-            <MyNavLink to="/skv-qr">Skattekonto QR</MyNavLink>
-            <MyNavLink to="/bokio-backup">Bokio Backup</MyNavLink>
-            <MyNavLink to="/december-lon">Decemberlön</MyNavLink>
-            {/* <MyNavLink to="/moms">Momssnurra</MyNavLink> */}
-          </List>
-        </Drawer>
-        <Box
-          component="main"
-          sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
-        >
-          <Toolbar />
-          <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/invoice-qr" element={<InvoiceQRCodePage />} />
-              <Route path="/skv-qr" element={<SKVQRCodePage />} />
-              <Route path="/bokio-backup" element={<BokioBackupPage />} />
-              <Route path="/moms" element={<MomsSnurraPage />} />
-              <Route
-                path="/december-lon"
-                element={<CalculateDecemberSalaryPage />}
-              />
-            </Routes>
-          </Suspense>
+              flexShrink: 0,
+              '& .MuiDrawer-paper': {
+                width: DrawerWidth,
+                boxSizing: 'border-box',
+              },
+            }}
+          >
+            <Toolbar />
+            <Divider />
+            <List>
+              <MyNavLink to="/">Hem</MyNavLink>
+              {/* <MyNavLink to="/invoice-qr">Invoice QR</MyNavLink> */}
+              <MyNavLink to="/skv-qr">Skattekonto QR</MyNavLink>
+              <MyNavLink to="/bokio-backup">Bokio Backup</MyNavLink>
+              <MyNavLink to="/december-lon">Decemberlön</MyNavLink>
+              {/* <MyNavLink to="/moms">Momssnurra</MyNavLink> */}
+            </List>
+          </Drawer>
+          <Box
+            component="main"
+            sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+          >
+            <Toolbar />
+            <Suspense fallback={<div>Loading...</div>}>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/invoice-qr" element={<InvoiceQRCodePage />} />
+                <Route path="/skv-qr" element={<SKVQRCodePage />} />
+                <Route path="/bokio-backup" element={<BokioBackupPage />} />
+                <Route path="/moms" element={<MomsSnurraPage />} />
+                <Route
+                  path="/december-lon"
+                  element={<CalculateDecemberSalaryPage />}
+                />
+              </Routes>
+            </Suspense>
+          </Box>
         </Box>
-      </Box>
-    </Router>
+      </Router>
+    </ThemeProvider>
   );
 }
 
